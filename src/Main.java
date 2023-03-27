7292915dc1313348060efb6d9a86f496381d3d3b
fraser <fraser.hobbs@sky.com>
2023-03-27 15:10:58 +0100

    Updated database to hold (deviceID, Processor, Ram, systemType, roomID, selectedID)
    Updated UI interface to account for database changes.
    
    Added Inventory.db as example data
    
    QOL Changes:
    - Updating records now no longer shows old values before clearing popup
    
    - Set Preferred Widths for table columns

--- src/Main.java
+++ src/Main.java
@@ -17,59 +17,86 @@ import java.sql.*;
 import java.util.Objects;
 
 public class Main {
-//    Global Variables
+    //    Global Variables
     boolean firstConnect = true;
     String selectedID = null;
-//    UI Components
 
+//----------------------------------------------------------------------------------------------------------------------
+
+//   UI Components
+
+    //    JPanels
     private JPanel Main;
-    private JTable tbl_Inventory;
-    private JTextField txt_ComputerID;
-    private JButton btn_Add;
-    private JButton btn_Update;
-    private JButton btb_Delete;
-    private JTextField txt_Search;
     @SuppressWarnings("unused")
     private JPanel infoPanel;
-    private JTextArea txt_Software;
-    private JTextArea txt_Hardware;
-    private JButton btn_Clear;
-    private JComboBox<String> searchOptions;
     @SuppressWarnings("unused")
     private JPanel panel_left;
     @SuppressWarnings("unused")
     private JPanel panel_right;
     @SuppressWarnings("unused")
+    private JPanel deviceID;
+    @SuppressWarnings("unused")
+    private JPanel roomID;
+    @SuppressWarnings("unused")
+    private JPanel Ram;
+    private JPanel Processor;
+    @SuppressWarnings("unused")
+    private JPanel systemType;
+    @SuppressWarnings("unused")
+    private JPanel ButtonPanel;
+
+    //  JTextFields, ComboBox's & TextAreas
+    private JTextField txt_DeviceID;
+    private JTextField txt_roomID;
+    private JTextField txt_Ram;
+    private JTextField txt_Processor;
+    private JTextArea txt_systemType;
+    private JTextField txt_Search;
+    private JComboBox<String> searchOptions;
+
+
+    //  JButtons
+    private JButton btn_Add;
+    private JButton btn_Update;
+    private JButton btb_Delete;
+    private JButton btn_Clear;
+
+    //  JTable & JScrollPane
+    private JTable tbl_Inventory;
+    @SuppressWarnings("unused")
     private JScrollPane tbl_Inventory_Scroll;
 
+    //----------------------------------------------------------------------------------------------------------------------
     public Main() {
         connect();
         loadTable(null);
 
 //      Create Search Categories for JComboBox
-        String[] searchCategories = {"computerID", "Software", "Hardware"};
+        String[] searchCategories = {"deviceID", "Processor", "Pro", "systemType", "roomID"};
         for (String category : searchCategories) {
             searchOptions.addItem(category);
         }
 //      Set Default Selection for JComboBox
         searchOptions.setSelectedItem(searchCategories[0]);
 
-//      Add Button Handler
+//      Button - ADD Handler
         btn_Add.addActionListener(e -> {
-            String ID = txt_ComputerID.getText();
-            String Software = txt_Software.getText();
-            String Hardware = txt_Hardware.getText();
+            String ID = txt_DeviceID.getText();
+            String Processor = txt_Processor.getText();
+            String Ram = txt_Ram.getText();
+            String systemType = txt_systemType.getText();
+            String roomID = txt_roomID.getText();
             if (ID.length() < 1) {
                 JOptionPane.showMessageDialog(null, "Record Failed To Add\nName Field Needs Completed");
             } else {
-                insert(ID, Software, Hardware);
+                insert(ID, Processor, Ram, systemType, roomID);
 
                 loadTable(null);
             }
 
         });
 
-//      Table Double Click Handler
+//      TABLE -  Double-Click Handler
         tbl_Inventory.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
                 if (e.getClickCount() == 2) {
@@ -81,50 +108,55 @@ public class Main {
                 }
             }
         });
-//      Listens For Change in Search Field
-        addChangeListener(txt_Search, e -> loadTable(txt_Search.getText()));
 
+//      SEARCH - Change Handler
+        addChangeListener(txt_Search, e -> loadTable(txt_Search.getText()));
         searchOptions.addActionListener(e -> loadTable(txt_Search.getText()));
 
+
+//      BUTTON - Delete Handler
         btb_Delete.addActionListener(e -> delete(selectedID));
-
+//      BUTTON - Update Handler
         btn_Update.addActionListener(e -> update(selectedID));
-
+//      BUTTON - Clear Handler
         btn_Clear.addActionListener(e -> clear());
     }
 
 
     public static void main(String[] args) {
+//      JFrame Setup & Initialisation
         JFrame frame = new JFrame("Inventory Manager");
-
         frame.setContentPane(new Main().Main);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
 
+    //    Database - Connect & Create
     private Connection connect() {
         String URL = "jdbc:sqlite:data/Inventory.db";
         Connection conn = null;
         String SQL = """
                 CREATE TABLE IF NOT EXISTS Inventory (
                     _ID    INTEGER PRIMARY KEY AUTOINCREMENT default 0 NOT NULL,
-                    computerID        TEXT              not null,
-                    Software  TEXT,
-                    Hardware  TEXT
+                    deviceID        TEXT              not null,
+                    Processor  TEXT,
+                    Ram  TEXT,
+                    systemType TEXT,
+                    roomID TEXT
                 );""";
 
         try {
             conn = DriverManager.getConnection(URL);
 
             if (conn != null && firstConnect) {
-//                Outputs for Testing
-//                DatabaseMetaData meta = conn.getMetaData();
-//                System.out.println("Driver name: " + meta.getDriverName());
-//                System.out.println("Driver version: " + meta.getDriverVersion());
-//                System.out.println("Product name: " + meta.getDatabaseProductName());
-//                System.out.println("Product version: " + meta.getDatabaseProductVersion());
-//                System.out.println("Connected To DataBase.");
+//              Database Driver Info
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("Driver name: " + meta.getDriverName());
+                System.out.println("Driver version: " + meta.getDriverVersion());
+                System.out.println("Product name: " + meta.getDatabaseProductName());
+                System.out.println("Product version: " + meta.getDatabaseProductVersion());
+                System.out.println("Connected To DataBase.");
                 Statement stmt = conn.createStatement();
                 {
                     stmt.execute(SQL);
@@ -139,18 +171,20 @@ public class Main {
         return conn;
     }
 
-    public void insert(String ID, String Software, String Hardware) {
+    public void insert(String ID, String Processor, String Ram, String systemType, String roomID) {
 
         if (search(ID)) {
-            JOptionPane.showMessageDialog(null, "Computer ID Already Exists");
+            JOptionPane.showMessageDialog(null, "Device ID Already Exists");
         } else {
-            String SQL = "INSERT INTO Inventory(computerID, Software, Hardware) VALUES(?,?,?)";
+            String SQL = "INSERT INTO Inventory(deviceID, Processor, Ram, systemType, roomID) VALUES(?,?,?,?,?)";
 
             try (Connection conn = this.connect();
                  PreparedStatement stmt = conn.prepareStatement(SQL)) {
                 stmt.setString(1, ID);
-                stmt.setString(2, Software);
-                stmt.setString(3, Hardware);
+                stmt.setString(2, Processor);
+                stmt.setString(3, Ram);
+                stmt.setString(4, systemType);
+                stmt.setString(5, roomID);
                 stmt.executeUpdate();
                 JOptionPane.showMessageDialog(null, "Record Successfully Added!");
                 clear();
@@ -163,27 +197,31 @@ public class Main {
     }
 
     public void update(String selectedID) {
-        String computerID = txt_ComputerID.getText();
-        String Software = txt_Software.getText();
-        String Hardware = txt_Hardware.getText();
+        String deviceID = txt_DeviceID.getText();
+        String Processor = txt_Processor.getText();
+        String Ram = txt_Ram.getText();
+        String systemType = txt_systemType.getText();
+        String roomID = txt_roomID.getText();
 
-        if (!Objects.equals(selectedID, computerID) && search(computerID) && selectedID != null) {
-            JOptionPane.showMessageDialog(null, "Computer ID Already In Use!");
+        if (!Objects.equals(selectedID, deviceID) && search(deviceID) && selectedID != null) {
+            JOptionPane.showMessageDialog(null, "Device ID Already In Use!");
         } else {
             if (search(selectedID)) {
-                String SQL = "UPDATE Inventory SET computerID = ?,Software = ?,Hardware = ? WHERE computerID = ?";
-
+                String SQL = "UPDATE Inventory SET deviceID = ?,Processor = ?,Ram = ?, systemType = ?, roomID = ? WHERE deviceID = ?";
                 try {
                     Connection conn = this.connect();
                     PreparedStatement stmt = conn.prepareStatement(SQL);
-                    stmt.setString(1, computerID);
-                    stmt.setString(2, Software);
-                    stmt.setString(3, Hardware);
-                    stmt.setString(4, selectedID);
+                    stmt.setString(1, deviceID);
+                    stmt.setString(2, Processor);
+                    stmt.setString(3, Ram);
+                    stmt.setString(4, systemType);
+                    stmt.setString(5, roomID);
+                    stmt.setString(6, selectedID);
                     stmt.executeUpdate();
-                    loadTable(null);
-                    JOptionPane.showMessageDialog(null, "Record Updated!");
                     clear();
+                    loadTable(null);
+                    search(selectedID);
+                    JOptionPane.showMessageDialog(null, "Record Updated!");
                 } catch (SQLException e) {
                     JOptionPane.showMessageDialog(null, "Record Could Not Be Updated");
                     System.out.println(e.getMessage());
@@ -194,7 +232,7 @@ public class Main {
     }
 
     public void delete(String selectedID) {
-        String SQL = "DELETE FROM Inventory WHERE computerID = ?";
+        String SQL = "DELETE FROM Inventory WHERE deviceID = ?";
 
         try {
             Connection conn = this.connect();
@@ -214,26 +252,32 @@ public class Main {
 
     public void clear() {
         selectedID = null;
-        txt_ComputerID.setText("");
-        txt_Software.setText("");
-        txt_Hardware.setText("");
+        txt_DeviceID.setText("");
+        txt_Processor.setText("");
+        txt_Ram.setText("");
+        txt_systemType.setText("");
+        txt_roomID.setText("");
     }
 
     public boolean search(String searchID) {
         try {
 
-            PreparedStatement stmt = connect().prepareStatement("SELECT computerID, Software, Hardware from Inventory WHERE computerID LIKE ?");
+            PreparedStatement stmt = connect().prepareStatement("SELECT deviceID, Processor, Ram, systemType, roomID from Inventory WHERE deviceID LIKE ?");
             stmt.setString(1, searchID);
             ResultSet resultSet = stmt.executeQuery();
 
             if (resultSet.next()) {
-                String computerID = resultSet.getString(1);
-                String computerSoftware = resultSet.getString(2);
-                String computerHardware = resultSet.getString(3);
+                String deviceID = resultSet.getString(1);
+                String Processor = resultSet.getString(2);
+                String Ram = resultSet.getString(3);
+                String systemType = resultSet.getString(4);
+                String roomID = resultSet.getString(5);
 //              If Results found display the fields
-                txt_ComputerID.setText(computerID);
-                txt_Software.setText(computerSoftware);
-                txt_Hardware.setText(computerHardware);
+                txt_DeviceID.setText(deviceID);
+                txt_Processor.setText(Processor);
+                txt_Ram.setText(Ram);
+                txt_systemType.setText(systemType);
+                txt_roomID.setText(roomID);
                 stmt.close();
                 resultSet.close();
                 return true;
@@ -253,13 +297,13 @@ public class Main {
         try {
             PreparedStatement stmt;
             if (SearchCriteria != null) {
-                stmt = connect().prepareStatement("SELECT computerID, Software, Hardware from Inventory WHERE " + searchOptions.getSelectedItem() + " LIKE ? ORDER BY computerID");
+                stmt = connect().prepareStatement("SELECT deviceID, Processor, Ram, systemType, roomID from Inventory WHERE " + searchOptions.getSelectedItem() + " LIKE ? ORDER BY deviceID");
                 stmt.setString(1, ("%" + SearchCriteria + "%"));
                 ResultSet rs = stmt.executeQuery();
                 tbl_Inventory.setModel(DbUtils.resultSetToTableModel(rs));
 
             } else {
-                stmt = connect().prepareStatement("SELECT computerID, Software, Hardware from Inventory ORDER BY computerID");
+                stmt = connect().prepareStatement("SELECT deviceID, Processor, Ram, systemType, roomID from Inventory ORDER BY deviceID");
                 ResultSet rs = stmt.executeQuery();
                 tbl_Inventory.setModel(DbUtils.resultSetToTableModel(rs));
             }
@@ -273,12 +317,18 @@ public class Main {
         } finally {
             JTableHeader header = tbl_Inventory.getTableHeader();
             TableColumnModel colMod = header.getColumnModel();
-            String[] colNames = {"Computer ID", "Software", "Hardware"};
+            String[] colNames = {"Device ID", "Processor", "RAM", "System Type", "Room ID"};
             for (int i = 0; i < tbl_Inventory.getColumnCount(); i++) {
                 TableColumn tabCol = colMod.getColumn(i);
                 tabCol.setHeaderValue(colNames[i]);
                 header.repaint();
             }
+            //      Set Table Column Preferred Widths
+            tbl_Inventory.getColumnModel().getColumn(0).setPreferredWidth(280);
+            tbl_Inventory.getColumnModel().getColumn(1).setPreferredWidth(280);
+            tbl_Inventory.getColumnModel().getColumn(2).setPreferredWidth(60);
+            tbl_Inventory.getColumnModel().getColumn(3).setPreferredWidth(450);
+            tbl_Inventory.getColumnModel().getColumn(4).setPreferredWidth(280);
         }
 
 
